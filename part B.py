1c3a9f57640eaee90bd50db370647bc91c9530b5
harshpatel44 <harshgp44@gmail.com>
2020-05-21 22:18:33 -0300

    acl permission changed

--- part B.py
+++ part B.py
@@ -30,89 +30,34 @@ import boto3
 
 
 """get ACL permissions"""
-# s3Client = boto3.client('s3')
-# print(s3Client.get_bucket_acl(Bucket='second-bucket-csci-5410'))
+s3Client = boto3.client('s3')
+#print(s3Client.get_bucket_acl(Bucket='second-bucket-csci-5410'))
 
-
-
-"""set policy to block public access"""
-import json
-#s3Client = boto3.resource('s3')
-#s3Client = boto3.client('s3')
-
-#
-# policy={
-#   "Id": "Policy1590045787132",
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Sid": "Stmt1590045784956",
-#       "Action": [
-#         "s3:PutPublicAccessBlock"
-#       ],
-#       "Effect": "Deny",
-#       "Resource": "arn:aws:s3:::fifth-bucket-csci-5410",
-#       "Principal": "*"
-#     }
-#   ]
-# }
-# policy=json.dumps(policy)
-# s3Client.put_bucket_policy(Bucket='fifth-bucket-csci-5410',Policy=policy)
-
-# response = s3Client.get_public_access_block(Bucket='second-bucket-csci-5410')
-# print(response)
-#
-
-
-"""ACL"""
-
-# s3Client = boto3.client('s3')
-# policy={
-#     "Version": "2012-10-17",
-#     "Id": "Policy1590103199246",
-#     "Statement": [
-#         {
-#             "Sid": "Stmt1590103198148",
-#             "Effect": "Allow",
-#             "Principal": "*",
-#             "Action": "*",
-#             "Resource": "arn:aws:s3:::second-bucket-csci-5410"
-#         }
-#     ]
-# }
-# policy=json.dumps(policy)
-# s3Client.put_bucket_policy(Bucket='second-bucket-csci-5410',Policy=policy)
-#s3Client.put_bucket_acl(Bucket='second-bucket-csci-5410', ACL='private')
+"""Set ACL permissions"""
+s3Client.put_bucket_acl(
+    Bucket='second-bucket-csci-5410',
+    AccessControlPolicy={
+        'Grants': [
+            {
+                'Grantee': {
+                    'DisplayName': 'awslabsc0w758896t1588300632',
+                    # 'EmailAddress': '',
+                    'ID': '303d17372927d47a314483c707fd247cacfb8334cbbb190f9477a980ffdf3c5a',
+                    'Type': 'CanonicalUser',
+                    # 'URI': 'string'
+                },
+                'Permission': 'READ_ACP'
+            },
+        ],
+        'Owner': {
+            'DisplayName': 'awslabsc0w758896t1588300632',
+            'ID': '303d17372927d47a314483c707fd247cacfb8334cbbb190f9477a980ffdf3c5a'
+        }
+    },
+)
+print(s3Client.get_bucket_acl(Bucket='second-bucket-csci-5410'))
 
 
 
 
 
-
-# s3Client = boto3.client('s3')
-# response = s3Client.put(
-#     ACL='private'|'public-read'|'public-read-write'|'authenticated-read',
-#     AccessControlPolicy={
-#         'Grants': [
-#             {
-#                 'Grantee': {
-#                     'DisplayName': 'string',
-#                     'EmailAddress': 'string',
-#                     'ID': 'string',
-#                     'Type': 'CanonicalUser'|'AmazonCustomerByEmail'|'Group',
-#                     'URI': 'string'
-#                 },
-#                 'Permission': 'FULL_CONTROL'|'WRITE'|'WRITE_ACP'|'READ'|'READ_ACP'
-#             },
-#         ],
-#         'Owner': {
-#             'DisplayName': 'string',
-#             'ID': 'string'
-#         }
-#     },
-#     GrantFullControl='string',
-#     GrantRead='string',
-#     GrantReadACP='string',
-#     GrantWrite='string',
-#     GrantWriteACP='string'
-# )
